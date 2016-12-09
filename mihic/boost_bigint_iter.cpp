eeda645fbe8440b1dc71736dfb08b9d81e0d39b1
mihic <miha.elersic@gmail.com>
2016-12-09 17:16:05 +0100

    cleanup

--- mihic/boost_bigint_iter.cpp
+++ mihic/boost_bigint_iter.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
       number<gmp_int> a=1;
       number<gmp_int> b=1;
       printf("Calculating %dth fibbonaci number\n",num_iters);
-      num_iters = num_iters +1;
+      num_iters = num_iters -1;
       for (int i = 0; i<num_iters;i++){
          a+=b;
          std::swap(a,b);
