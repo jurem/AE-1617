d550440d7265e0ef9da714c1867fdf7d624a76fc
mihic <miha.elersic@gmail.com>
2016-12-09 17:06:54 +0100

    pohitritev do verzije s pointerji (da je isto)

--- mihic/boost_bigint_iter.cpp
+++ mihic/boost_bigint_iter.cpp
@@ -9,23 +9,16 @@ int main(int argc, char* argv[])
       int num_iters = std::atoi(argv[1]);
       number<gmp_int> a=1;
       number<gmp_int> b=1;
-      number<gmp_int> c=1;
-      //number<gmp_int> d=1;
-
-      //scanf("%d",&num_iters);
       printf("Calculating %dth fibbonaci number\n",num_iters);
       num_iters = num_iters +1;
       for (int i = 0; i<num_iters;i++){
-         c=a+b;
-         // a=b;
-         // b=c;
-         a=std::move(b);
-         b=std::move(c);
+         a+=b;
+         std::swap(a,b);
       }
 
-      c=c%1000000;
+      b=b%1000000;
       printf("Done\n");
-      std::cout << c << '\n';
+      std::cout << b << '\n';
       
    return 0;
 }
